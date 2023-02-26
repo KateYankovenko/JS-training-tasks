4677454514f77f4798d57eeb899643365db8ecd1
Kateryna Yankovenko <k.yankovenko@ukr.net>
2023-02-26 12:38:15 +0100

    closures

--- arrow-fns.js
+++ arrow-fns.js
@@ -1,13 +1,68 @@
 "use strict";
 
-const fnA = function (str, callback) {//2)here fnB is a parameter for fnA
-    console.log("Here is your string:", str)
-    console.log(callback)//it is not an f-ion calling, shows only fn's body
-    callback(123,789,654);//3)here passed params for fnB
-}
+// const fnA = function (str, callback) {//2)here fnB is a parameter for fnA
+//     console.log("Here is your string:", str)
+//     console.log(callback)//it is not an f-ion calling, shows only fn's body
+//     callback(123,789,654);//3)here passed params for fnB
+// }
 
-const fnB = function (...params) {//4)[123,789,654]
-    console.log("Callback fn:", params)//5)logging params
-}
+// const fnB = function (...params) {//4)[123,789,654]
+//     console.log("Callback fn:", params)//5)logging params
+// }
 
-fnA("Hope is here for u!", fnB)//1)calling of funct.
+// fnA("Hope is here for u!", fnB)//1)calling of funct.
+
+
+//Reusable code logic:
+// const doMath = function (a, b, callback) {
+//     console.log("My numbers:", a, b);
+
+//     const result = callback(a, b);
+//     console.log(result)
+// };
+
+// const add = (a, b) => a + b;
+
+// const subtract = (a, b) => a - b;
+
+// doMath(56, 7, add);
+// doMath(56, 7, subtract);
+
+
+//Ivent listener function:
+// const buttonRef = document.querySelector(".button");
+// // console.dir(buttonRef)//button is a js object
+
+// const handleBtnClick = function () {
+//     console.log("Button is clicked!")
+// }
+
+// buttonRef.addEventListener("click", handleBtnClick);
+
+
+const numbers = [65, 7, 2, 1, 0, 3, 4, 56]
+
+// numbers.forEach((number, index) => {
+//     console.log(`There are el and indexes!`, number, index)
+// })
+
+
+/*
+ * Фильтр
+ */
+
+const filter = function (array, test) {
+    const filteredArray = [];
+
+    for (const el of array) {
+        console.log(el);
+        const passed = test(el);
+
+        if (passed) {
+            filteredArray.push(el);
+        }
+    }
+
+    return filteredArray;
+};
+
---
+++ closures.js
@@ -0,0 +1,191 @@
+"use strict";
+
+/*
+ * Функция результатом своей работы может возвращать другую функцию.
+ *
+ * Возвращаемая функция во время вызова будет иметь доступ
+ * ко всем локальным переменным (области видимости)
+ * родительской функции (той из которой её вернули),
+ * это называется «замыкание».
+ */
+
+
+// 1. надо передать функцию
+// 2. функция получает элемент массива
+// 3. если элемент массива удовлетворяет условию то функция вернет true
+// 3. если элемент массива НЕ удовлетворяет условию то функция вернет false
+
+// const callback1 = function (value) {
+//     return value >= 3;
+// };
+
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
+
+// const callback2 = function (value) {
+//     return value <= 4;
+// };
+
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+// console.log(r2);
+
+// const fruits = [
+//     { name: 'apples', quantity: 200, isFresh: true },
+//     { name: 'grapes', quantity: 150, isFresh: false },
+//     { name: 'bananas', quantity: 100, isFresh: true },
+// ];
+
+// const getFruitsWithQuantity = function (fruit) {
+//     return fruit.quantity >= 120;
+// };
+
+// const r3 = filter(fruits, getFruitsWithQuantity);
+// console.log(r3);
+
+
+
+const fnA = function (parameter) {
+    const innerVariable = 'значение fnA';
+
+    const innerFunction = function () {
+        console.log(parameter);
+        console.log(innerVariable);
+        console.log('Это вызов innerFunction');
+    };
+
+    return innerFunction;
+};
+
+// const fnB = fnA(555);
+
+// fnB();
+
+// console.log(fnB);
+
+
+
+/*
+ * Dish maker
+ */
+// const makeDish = function (sheffName, dish) {
+//     console.log(`${sheffName} cooking ${dish}`);
+// };
+
+// makeDish('Mango', 'омлет');
+// makeDish('Mango', 'чай');
+
+// makeDish('Poly', 'котлеты');
+// makeDish('Poly', 'кофе');
+
+const makeSheff = function (name) {
+    const innverVar = 555;
+    const message = 'hello';
+
+    const makeDish = function (dish) {
+        console.log(message);
+        console.log(innverVar);
+        console.log(`${name} готовит ${dish}`);
+    };
+
+    return makeDish;
+};
+
+// const mango = makeSheff('Mango');
+
+// console.dir(mango);
+
+// mango('котлеты');
+
+// const poly = makeSheff('Poly');
+
+// console.dir(poly);
+
+// poly('чай');
+
+// console.dir(mango);
+
+
+/*
+ * Округлятор 
+ */
+
+// const floatingPoint = 3.456789;
+// const someInt = Math.round(floatingPoint); // 3
+// const withDecimals = Number(floatingPoint.toFixed(2)); // 3.46
+
+// const rounder = function (number, places) {
+//     return Number(number.toFixed(places));
+// };
+
+// console.log(rounder(3.4567, 2));
+// console.log(rounder(3.4567, 3));
+
+const rounder = function (places) {
+    return function (number) {
+        return Number(number.toFixed(places));
+    };
+};
+
+// const rounder2 = rounder(2);
+
+// console.dir(rounder2);
+
+// console.log(rounder2(3.4567));
+// console.log(rounder2(5.4512312312367));
+
+
+/*
+ * Приватные данные и функции - скрытие реализации, интерфейс
+ */
+
+const salaryManagerFactory = function (employeeName, baseSalary = 0) {
+    let salary = baseSalary;
+
+    return {
+        raise(amount) {
+            if (amount > 1000) {
+                return 'Too much';
+            }
+
+            salary += amount;
+        },
+        lower(amount) {
+            salary -= amount;
+        },
+        current() {
+            return `Salary nowadays is ${employeeName} - ${salary}`;
+        },
+    };
+};
+
+const salaryManager = salaryManagerFactory('Mango', 5000);
+
+console.log(salaryManager.current());
+
+console.log(salaryManager.raise(10000000));
+
+console.log(salaryManager.current());
+
+// const myLibFactory = function () {
+//     let value = 0;
+
+//     const add = function (num) {
+//         value += num;
+//     };
+
+//     return {
+//         add: add,
+//         getValue() {
+//             return value;
+//         },
+//     };
+// };
+
+// const myLib = myLibFactory();
+
+// console.dir(myLib.getValue);
+
+// console.log(myLib);
+// console.log(myLib.getValue());
+// myLib.add(10);
+// console.log(myLib.getValue());
