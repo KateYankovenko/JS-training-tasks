4efafb58c91d709fe4be741d6eea49f19c805d70
Kateryna Yankovenko <k.yankovenko@ukr.net>
2023-03-07 22:27:09 +0100

    arr. metds

---
+++ array_methods.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// Дан массив с числами. Создайте новый массив, состоящий из квадратов этих чисел.
+// const numbers = [23, 67, 89, 0, -1, 4];
+// const sorted = [];
+// numbers.forEach((number, numbers) => sorted.push(number ** 2))
+// console.log(sorted)
+
+
+// Дан массив с числами. Найдите сумму этих чисел.
+// const numbers = [23, 67, 89, 0, -1, 4];
+// let total = 0;
+// numbers.forEach(number => total += number)
+// console.log(total)
+
+
+// Дан массив с числами. Сделайте из него массив, состоящий из квадратов этих чисел.
+const numbers = [23, 67, 89, 0, -1, 4];
+const sqrtArr = [];
+numbers.map()
+
--- closures.js
+++ closures.js
@@ -240,3 +240,49 @@ const rounder = function (places) {
 //  const c3 = increment()
 //  console.log('example increment', c1, c2, c3)
 
+
+// let addArr = [1, 3, 4, 5]
+// let newArray = []
+// addArr.forEach((element, index) => addArr[index]=element * 2)
+// // console.log(newArray)
+// console.log(addArr)
+
+
+// const students = [
+//   { name: "Манго", courses: ["математика", "фізика"] },
+//   { name: "Полі", courses: ["інформатика", "математика"] },
+//   { name: "Ківі", courses: ["фізика", "біологія"] },
+// ];
+
+// const allCourses = students.flatMap(student => student.courses);
+// // ['математика', 'фізика', 'інформатика', 'математика', 'фізика', 'біологія'];
+// // const allCourses1 = students.map(student => student.courses);
+// // console.log(allCourses)
+// // console.log(allCourses1)
+// const uniqueCourses = allCourses.filter(
+//   (course, index, array) => array.indexOf(course) === index
+// );
+// console.log(uniqueCourses)
+
+
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Манго", score: 83 },
+  { name: "Полі", score: 59 },
+  { name: "Аякс", score: 37 },
+  { name: "Ківі", score: 94 },
+  { name: "Х'юстон", score: 64 },
+];
+
+const best = students.filter(student => student.score >= HIGH_SCORE);
+console.log(best); // Масив об'єктів з іменами Манго і Ківі
+
+const worst = students.filter(student => student.score < LOW_SCORE);
+console.log(worst); // Масив з одним об'єктом Аякс
+
+// В колбек-функції зручно деструктуризувати властивості об'єкта
+const average = students.filter(
+  ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
+);
+console.log(average); // Масив об'єктів з іменами Полі і Х'юстон
