5a0b19183b9b96cefd06909985877cbdadae8693
Kateryna Yankovenko <k.yankovenko@ukr.net>
2023-03-12 23:23:32 +0100

    accumulator for tags count

--- array_methods.js
+++ array_methods.js
@@ -106,19 +106,67 @@
 /*
  * Собираем все теги из твитов
  */
-// const tweets = [
-//   { id: '000', likes: 5, tags: ['js', 'nodejs'] },
-//   { id: '001', likes: 2, tags: ['html', 'css'] },
-//   { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
-//   { id: '003', likes: 8, tags: ['css', 'react'] },
-//   { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
-// ];
+const tweets = [
+  { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+  { id: '001', likes: 2, tags: ['html', 'css'] },
+  { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+  { id: '003', likes: 8, tags: ['css', 'react'] },
+  { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+];
 
-// const allTags = tweets.flatMap(tweet => tweet.tags)//['js', 'nodejs', 'html', 'css', 'html', 'js', 'nodejs', 'css', 'react', 'js', 'nodejs', 'react']
-// const uniqueTags = allTags.filter((tag,index,array) => array.indexOf(tag)===index)
+const allTags = tweets.flatMap(tweet => tweet.tags)//['js', 'nodejs', 'html', 'css', 'html', 'js', 'nodejs', 'css', 'react', 'js', 'nodejs', 'react']
+const uniqueTags = allTags.filter((tag,index,array) => array.indexOf(tag)===index)//['js', 'nodejs', 'html', 'css', 'react']
 
 // console.log(allTags)
 // console.log(uniqueTags)
+
+
+
+
+// Винесемо callback-функцію окремо, а в reducе передамо посилання на неї.
+// Це стандартна практика, якщо callback-функція досить велика.
+
+// Мабуть, збирання тегів - не одиночна операція, тому напишемо функцію
+// для збирання тегів з колекції
+// const getTags = tweets =>
+//   tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
+
+//     return allTags;
+//   }, []);
+
+// console.log(getTags(tweets));
+
+
+// Якщо в об'єкті-акумуляторі acc відсутня своя властивість з ключем tag,
+// то створюємо її і записуємо їй значення 0.
+// В іншому випадку збільшуємо значення на 1.
+
+const tags = tweets.reduce((allTags, tweet) => {
+  allTags.push(...tweet.tags);
+
+  return allTags;
+}, []);
+
+console.log(tags);
+
+const getTagStats = (acc, tag) => {
+  if (!acc.hasOwnProperty(tag)) {
+    acc[tag] = 0;
+  }
+
+  acc[tag] += 1;
+
+  return acc;
+};
+
+// Початкове значення акумулятора - це порожній об'єкт {}
+const countTags = tags => tags.reduce(getTagStats, {});
+
+const tagCount = countTags(tags);
+console.log(tagCount);
+
+
 
 
 
